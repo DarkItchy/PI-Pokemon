6e16f883ef4ae0c426cc89a83bf69c47a0b6609e
Itchy <JimenezGalvezEduardo@gmail.com>
2023-01-17 14:47:13 -0600

    Progreso PI Back-end(Llamada a la api)

--- api/src/Utils/index.js
+++ api/src/Utils/index.js
@@ -1,35 +1,71 @@
 const axios = require("axios");
 const { Pokemon, Type } = require("../db");
 
-const apiInfo = async () => {
+// const apiInfo = async () => {
+//   try {
+//     const response = await axios.get(
+//       "https://pokeapi.co/api/v2/pokemon?offset=0&limit=30",
+//       { headers: { "Accept-Encoding": "gzip,deflate,compress" } }
+//     );
+//     const allInfo = await Promise.all(
+//       response.data.results.map(async (el) => {
+//         const info = await axios.get(el.url);
+//         const data = info.data;
+//         return {
+//           name: data.name,
+//           id: data.id,
+//           hp: data.stats[0].base_stat,
+//           atk: data.stats[1].base_stat,
+//           def: data.stats[2].base_stat,
+//           spatk: data.stats[3].base_stat,
+//           spdef: data.stats[4].base_stat,
+//           speed: data.stats[5].base_stat,
+//           height: data.height,
+//           weight: data.weight,
+//           img: data.sprites.other["official-artwork"].front_default,
+//         };
+//       })
+//     );
+//     return allInfo;
+//   } catch (e) {
+//     console.log(e), "Error en la lógica";
+//   }
+// };
+
+const apiInfo = async (id) => {
   try {
-    const response = await axios.get(
-      "https://pokeapi.co/api/v2/pokemon?offset=0&limit=26"
-    );
-    const allInfo = await Promise.all(
-      response.data.results.map(async (el) => {
-        const info = await axios.get(el.url);
-        const data = info.data;
-        const pokemon = {
-          name: data.name,
-          id: data.id,
-          hp: data.stats[0].base_stat,
-          atk: data.stats[1].base_stat,
-          def: data.stats[2].base_stat,
-          spatk: data.stats[3].base_stat,
-          spdef: data.stats[4].base_stat,
-          speed: data.stats[5].base_stat,
-          height: data.height,
-          weight: data.weight,
-          img: data.sprites.other["official-artwork"].front_default,
-        };
-        return pokemon;
-      })
-    );
-    return allInfo;
+    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    const info = response.data;
+    return {
+      name: info.name,
+      id: info.id,
+      hp: info.stats[0].base_stat,
+      atk: info.stats[1].base_stat,
+      def: info.stats[2].base_stat,
+      spatk: info.stats[3].base_stat,
+      spdef: info.stats[4].base_stat,
+      speed: info.stats[5].base_stat,
+      height: info.height,
+      weight: info.weight,
+      img: info.sprites.other["official-artwork"].front_default,
+    };
   } catch (e) {
-    console.log(e);
+    console.log(e), "Error en la lógica";
   }
 };
 
-module.exports = { apiInfo };
+const counter = async () => {
+  try {
+    let id = 1;
+    let arr = [];
+    while (id < 41) {
+      arr.push(await apiInfo(id));
+      id++;
+    }
+    return arr;
+  } catch (e) {
+    console.log(e), "Error en las llamadas";
+  }
+};
+
+module.exports = { apiInfo, counter };
--- api/src/controllers/pokemonsControllers.js
+++ api/src/controllers/pokemonsControllers.js
@@ -1,11 +1,14 @@
-const { apiInfo } = require("../Utils");
+const { apiInfo, counter } = require("../Utils");
+const { Pokemon, Type} = require("../db");
 
 const getPokemons = async (req, res) => {
+  const name = req.query
   try {
-    const result = await apiInfo();
+    const result = await counter();
     res.json(result);
   } catch (e) {
-    res.send(e.toString());
+    res.send(e.toString()),
+    "Error en el controller"
   }
 };
 
